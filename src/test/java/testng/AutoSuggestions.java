1541f674988fa82edeac5bd867615a93ff3120e8
GadiparthiRevati <revathigadiparthii@gmail.com>
2024-10-22 15:53:45 +0530

    Update AutoSuggestions.java

--- src/test/java/testng/AutoSuggestions.java
+++ src/test/java/testng/AutoSuggestions.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
+//auto
 public class AutoSuggestions {
 	@Test
 	public void moblileTest() {
